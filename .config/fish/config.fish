cb6c3546641f7ce14b21b9577bdaa73cdc6d48d1
Oyanghd <oyanghd@gmail.com>
2023-12-30 20:54:32 +0800

    feat(.config): feat fish scripts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -gx PATH "$PATH:/home/oyhd/.cargo/bin/"
+
 starship init fish | source
 zoxide init fish | source
 
